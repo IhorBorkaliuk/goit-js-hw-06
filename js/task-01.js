26bbae7e138e04f14c38bca92229c806648c1874
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-02 19:35:56 +0200

    1 task

---
+++ js/task-01.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const amountCategories = document.querySelectorAll('.item')
+console.log('Number of categories', amountCategories.length)
+
+const titleText = document.querySelectorAll('h2')
+console.log('Category', titleText[0].textContent)
+
+const amountEls = document.querySelectorAll('ul')
+console.log('Elements', amountEls[1].children.length)
+
+console.log('Category', titleText[1].textContent)
+
+console.log('Elements', amountEls[2].children.length)
+
+console.log('Category', titleText[2].textContent)
+
+console.log('Elements', amountEls[3].children.length)
+
