eebcd30e5eed859d222067e90aa211146b2acddb
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-02 21:28:37 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,19 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.createElement('ul')
+console.log(ingredientsList)
+
+const ingredientCreate = ingredient => {
+
+const ingredientEl = document.createElement('li')
+ingredientEl.textContent = ingredient
+ingredientEl.classList.add('item')
+
+return ingredientEl
+}
+
+const partOf = ingredients.map(ingredientCreate);
+
+ingredientsList.append(...partOf)
