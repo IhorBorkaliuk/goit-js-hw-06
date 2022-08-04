925ff8bb9c4d86ff4468e852b1ecc9103609ffc1
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-04 21:08:38 +0200

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const inputEl = document.querySelector('#font-size-control')
+console.log(inputEl)
+
+const textEl = document.querySelector('#text')
+console.log(textEl)
+
+
+inputEl.addEventListener('input', event => {
+    textEl.style.fontSize = event.currentTarget.value + "px"
+})
