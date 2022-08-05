a5bd0e92fc3cba23cc87eb3dfcae88495093161e
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-05 17:15:51 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,32 @@
+const formEl = document.querySelector('.login-form')
 
+const mailEl = document.querySelector('[type="email"]')
+console.log(mailEl)
+
+const passwordEl = document.querySelector('[type="password"]')
+console.log(passwordEl)
+
+formEl.addEventListener('submit', onSubmitForm)
+
+function onSubmitForm(event) {
+    event.preventDefault()
+
+    const wayToElements = event.currentTarget.elements
+    console.log(wayToElements)
+
+    const mail = wayToElements.email.value;
+
+    const password = wayToElements.password.value;
+
+    if(mail === '' || password === '') {
+        alert('Заповніть, будь ласка, всі поля!')
+    }
+
+    event.currentTarget.reset()
+
+    const dataForm = {
+        mail, password
+    }
+    console.log(dataForm)
+
+}
