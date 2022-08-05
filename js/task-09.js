2a3c6b36503a758fb9c08eeab51c3b3d7184ecbe
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-05 17:39:12 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,21 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+const btnChangeColor = document.querySelector('.change-color')
+console.log(btnChangeColor)
+
+const findColor = document.querySelector('.color')
+console.log(findColor)
+
+const findBody = document.querySelector('body')
+console.log(findBody)
+
+btnChangeColor.addEventListener('click', onChangeColor)
+
+function onChangeColor() {
+  findBody.style.backgroundColor = getRandomHexColor()
+
+  findColor.textContent = getRandomHexColor()
+}
